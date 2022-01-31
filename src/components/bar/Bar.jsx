99804d86cd5538cee968587e9db865bab93dd8f5
Thinushi <thinupavani@gmail.com>
2022-01-31 13:53:32 +0530

    bar style update

--- src/components/bar/Bar.jsx
+++ src/components/bar/Bar.jsx
@@ -3,7 +3,9 @@ import "./Bar.css";
 
 function Bar() {
   return (
-  <div className='barContainer'></div>
+  <div className='barContainer'>
+    <p className='textB'>Anderson Mill store moved to 8650 Spicewood Springs #115 78759</p>
+  </div>
   );
 }
 
