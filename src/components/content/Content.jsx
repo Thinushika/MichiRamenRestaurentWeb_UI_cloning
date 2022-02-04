6d1a64ad542d6aafb60877913a164e63aa9ed784
Thinushi <thinupavani@gmail.com>
2022-02-04 21:52:37 +0530

    update

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import "./Content.css";
 
 function Content() {
+
   return (
-  <div className='Ccontainer'>
-      <div className="Mrow">
-          <img src="" alt="logo" />
-          <h3>Please select your store</h3>
-          <p>Extra charge for remaking orders placed at wrong location</p>
-      </div>
-  </div>
+    <div className='Ccontainer'>
+      <section className="Mrow">
+        <div className="flexBox">
+          <div className="Box">
+            <center><img src="https://michiramen.com/img/logo_trans.gif" alt="logo" /></center>
+            <br/>
+            <center><h2 className='h2Topic'>Please select your store</h2></center>
+            <center><p>Extra charge for remaking orders placed at wrong location</p></center>
+          </div>
+        </div>
+      </section>
+    </div>
   );
 }
 
