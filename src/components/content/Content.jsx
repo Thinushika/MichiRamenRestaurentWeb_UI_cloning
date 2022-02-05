e1c42be15aa6b2e7afe4842998672c9174f25ef4
Thinushi <thinupavani@gmail.com>
2022-02-05 20:21:05 +0530

    update

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -4,18 +4,16 @@ import "./Content.css";
 function Content() {
 
   return (
-    <div className='Ccontainer'>
       <section className="Mrow">
         <div className="flexBox">
           <div className="Box">
             <center><img src="https://michiramen.com/img/logo_trans.gif" alt="logo" /></center>
             <br/>
             <center><h2 className='h2Topic'>Please select your store</h2></center>
-            <center><p>Extra charge for remaking orders placed at wrong location</p></center>
+            <center><p className='paraM'>Extra charge for remaking orders placed at wrong location</p></center>
           </div>
         </div>
       </section>
-    </div>
   );
 }
 
