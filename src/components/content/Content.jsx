6cb75c34ab0d1fa106e4e35510472f5518c267c1
Thinushi <thinupavani@gmail.com>
2022-02-05 22:46:28 +0530

    update

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -1,12 +1,38 @@
-import React from 'react';
+import React from "react";
 import "./Content.css";
+import details from "./details.json";
 
 function Content() {
-
   return (
-  <>
-  
-  </>
+    <>
+      {details.map((post) => {
+        return (
+          <>
+            <h2>{post.title}</h2>
+            <p>
+              <u>Address</u> <br />
+              {post.addressline1} <br />
+              {post.addressline2} <br />
+              {post.addressline3}
+            </p>
+            <p>
+              <u>Phone Number</u> <br />
+              {post.phoneNo}
+            </p>
+            <p>
+              <u>Hourse of Operation</u> <br />
+              Open Daily <br />
+              11 am - 10 pm 
+            </p>
+            <p>
+              <u>Services</u> <br />
+              Take-Out <br />
+              3rd-party Delivery
+            </p>
+          </>
+        );
+      })}
+    </>
   );
 }
 
