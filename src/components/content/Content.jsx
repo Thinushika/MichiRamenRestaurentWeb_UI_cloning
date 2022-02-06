252b692780232d659480faf630f69f1135c34bdf
Thinushi <thinupavani@gmail.com>
2022-02-06 08:19:56 +0530

    update

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -5,33 +5,42 @@ import details from "./details.json";
 function Content() {
   return (
     <>
-      {details.map((post) => {
-        return (
-          <>
-            <h2>{post.title}</h2>
-            <p>
-              <u>Address</u> <br />
-              {post.addressline1} <br />
-              {post.addressline2} <br />
-              {post.addressline3}
-            </p>
-            <p>
-              <u>Phone Number</u> <br />
-              {post.phoneNo}
-            </p>
-            <p>
-              <u>Hourse of Operation</u> <br />
-              Open Daily <br />
-              11 am - 10 pm 
-            </p>
-            <p>
-              <u>Services</u> <br />
-              Take-Out <br />
-              3rd-party Delivery
-            </p>
-          </>
-        );
-      })}
+      <div className="addressContainer">
+
+        <div className="logoContainer">
+          Logo
+        </div>
+
+        <div className="boxA">
+            {details.map((post) => {
+              return (
+                <div className="BoxC">
+                  <h2>{post.title}</h2>
+                  <p>
+                    <u>Address</u> <br />
+                    {post.addressline1} <br />
+                    {post.addressline2} <br />
+                    {post.addressline3}
+                  </p>
+                  <p>
+                    <u>Phone Number</u> <br />
+                    {post.phoneNo}
+                  </p>
+                  <p>
+                    <u>Hourse of Operation</u> <br />
+                    Open Daily <br />
+                    11 am - 10 pm
+                  </p>
+                  <p>
+                    <u>Services</u> <br />
+                    Take-Out <br />
+                    3rd-party Delivery
+                  </p>
+                </div>
+              );
+            })}
+        </div>
+      </div>
     </>
   );
 }
