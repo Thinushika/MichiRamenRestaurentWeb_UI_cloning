e8791f5985fec3137ba7222727051be146c6e90a
Thinushi <thinupavani@gmail.com>
2022-02-06 09:21:10 +0530

    update

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -8,30 +8,37 @@ function Content() {
       <div className="addressContainer">
 
         <div className="logoContainer">
-          Logo
+          <div className="logo">
+            <img src="https://michiramen.com/img/logo_trans.gif" alt="logo" />
+          </div>
+            <div>
+            <h2 className="Mh2tag">Please Select Your Store</h2>
+            <p className="Mptag">Extra charge for remaking orders placed at wrong location</p>
+            </div>
         </div>
 
         <div className="boxA">
             {details.map((post) => {
               return (
                 <div className="BoxC">
-                  <h2>{post.title}</h2>
-                  <p>
+                  <h2 className="h2Design">{post.title}</h2>
+                  <div className="btnDiv"><button className="btn">Menu/Order</button></div>
+                  <p className="pDesign">
                     <u>Address</u> <br />
                     {post.addressline1} <br />
                     {post.addressline2} <br />
                     {post.addressline3}
                   </p>
-                  <p>
+                  <p className="pDesign">
                     <u>Phone Number</u> <br />
                     {post.phoneNo}
                   </p>
-                  <p>
+                  <p className="pDesign">
                     <u>Hourse of Operation</u> <br />
                     Open Daily <br />
                     11 am - 10 pm
                   </p>
-                  <p>
+                  <p className="pDesign">
                     <u>Services</u> <br />
                     Take-Out <br />
                     3rd-party Delivery
